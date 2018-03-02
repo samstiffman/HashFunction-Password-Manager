130fde3f045c48317e346b213be9dde67c3444f6
Sam Stiffman <samstiffman@gmail.com>
2018-03-02 14:17:10 -0600

    Update Seed.java

--- Seed.java
+++ Seed.java
@@ -19,11 +19,14 @@ public class Seed {
 		SecureRandom random3 = new SecureRandom();
 		random2.setSeed(random3.nextLong());
 		random.setSeed(random2.nextLong());
+		random3.setSeed(random.nextLong());
+		
 		long test = random.nextInt(4) + random2.nextInt(4) + random3.nextInt(4);
 		if(test == 0)
 			hashValue *= 10;
 		else
 			hashValue += test;
+		//Iterates through powers of 10 in order to build an 18 digit number
 		for (int i = 1; i < 18; i++) {
 			test = (long) (((Math.pow(10, i)) * random.nextInt(4)) + ((Math.pow(10, i)) * random2.nextInt(4)) + ((Math.pow(10, i)) * random3.nextInt(4)));
 			if(test == 0)
